8763ac2ba6deac970aa5c16dddb818548a597fd1
Luciano Morais <lucianosantosmorais3@gmail.com>
2025-02-20 23:35:16 -0300

    encapsulamento

---
+++ src/AlunoTestar.java
@@ -0,0 +1,13 @@
+public class AlunoTestar {
+    public static void main(String[] args) {
+
+        Aluno aluno1 = new Aluno();
+
+        aluno1.setNome("Mario");
+
+        System.out.println(aluno1.getNome());
+
+        aluno1.setIdade(24);
+
+    }
+}
